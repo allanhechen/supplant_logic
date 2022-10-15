7163187489f5611a33a598276053edeb911f4c82
DaDrunkCow <max.yang.6449@gmai.com>
2022-10-15 11:02:20 -0400

    link to google sheets glossary

--- Python Portion/AlgorithmPlaceHolder.py
+++ Python Portion/AlgorithmPlaceHolder.py
@@ -5,9 +5,11 @@ import constant
 with open("test.txt") as txt:
     text = txt.readlines()
 
-# TODO: as of currently, the phrases will be separated using periods, commas and sentence conjunctions
 # for now, I will implement the splitting using constant variables from another python file
+# TODO: implement more constant split variables as needed
 for i in constant.split:
     phrases = text.split(i)
     text = phrases
 
+# firstly look for specific derogatory words that are blatantly bad to say, these are hard coded
+# TODO: implement more words
--- Python Portion/constant.py
+++ Python Portion/constant.py
@@ -1,1 +1,10 @@
+# this file is used for constants values
+import pygsheets
+
+# variables for where to split text into phrases
 split = [", ", ". ", " and ", " but "]
+
+# import glossary from excel sheet
+sheets = pygsheets.authorize(service_file="glossary_key.json").open_by_key("1hsG7UiSjyaIEWVHGd2dHRJIkSrRBeO4IDpQGqp19D9o")
+
+sheets[0].update_value("test")
