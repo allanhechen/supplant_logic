09659d02da9e100060e1719c1b9402e3c4a7140c
xcvk <96631109+xcvk@users.noreply.github.com>
2022-10-15 19:02:21 -0400

    Update AlgorithmPlaceHolder.py

--- Python Portion/AlgorithmPlaceHolder.py
+++ Python Portion/AlgorithmPlaceHolder.py
@@ -1,42 +1,44 @@
-import Utilities
+#import Utilities
 
 # this python code will read a text file and find derogatory statements
-
-from typing import List
 import constant
 
-# get the text in a variable
 with open("test.txt") as txt:
    text = txt.readlines()
 
-# for now, I will implement the splitting using constant variables from another python file
-# TODO: implement more constant split variables as needed
-"""textchange = 
-textchange = list(textchange.split(" "))
+from typing import List
 
-# firstly look for specific derogatory words that are blatantly bad to say, these are hard coded
-# TODO: implement more words to glassary
-for i in phrases:
-    """
 
-ListOfWords = {}
-ListOfOthers = {}
-str = ""
-enum = 0
+def algor():
+    ListOfWords = {}
+    ListOfOthers = {}
+    str = ""
+    enum = 0
 
-for item in text:
-    if item.isalpha():
-        str += item.lower()
-    else:
-        if str != "":
-            ListOfWords[enum] = str
-            str = ""
+    for item in text:
+        if item.isalpha():
+            str += item.lower()
         else:
-            ListOfOthers[enum] = item;      
-    enum += 1
+            if str != "":
+                ListOfWords[enum] = str
+                enum += 1
+            str = ""
 
-for item in ListOfWords:
-    if constant.blacklist.get(ListOfWords[item]) != None:
-        ListOfWords[item] = constant.synonym[ListOfWords[item]]
+            ListOfOthers[enum] = item
 
+        
+            enum += 1
+    ListOfWords[enum] = str
+    """for item in ListOfWords:
+        if constant.blacklist.get(ListOfWords[item]) != None:
+            ListOfWords[item] = constant.synonym[ListOfWords[item]]"""
 
+    enum = 0
+    str = ""
+    while enum != len(ListOfOthers) + len(ListOfWords):
+        if enum in ListOfOthers:
+            str += ListOfOthers[enum]
+        else:
+            str += ListOfWords[enum]
+        enum += 1
+    
