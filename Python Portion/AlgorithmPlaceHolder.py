d1ac50dfbec3e4839db84e6dd1c7866f3ba828d7
xcvk <96631109+xcvk@users.noreply.github.com>
2022-10-15 16:57:10 -0400

    Update AlgorithmPlaceHolder.py

--- Python Portion/AlgorithmPlaceHolder.py
+++ Python Portion/AlgorithmPlaceHolder.py
@@ -1,18 +1,42 @@
 import Utilities
 
+# this python code will read a text file and find derogatory statements
+
+from typing import List
+import constant
+
 # get the text in a variable
 with open("test.txt") as txt:
-    text = txt.readlines()
+   text = txt.readlines()
 
 # for now, I will implement the splitting using constant variables from another python file
 # TODO: implement more constant split variables as needed
-for i in constant.split:
-    phrases = text.split(i)
-    text = phrases
-
-# translate the blacklisted variables
-
+"""textchange = 
+textchange = list(textchange.split(" "))
 
 # firstly look for specific derogatory words that are blatantly bad to say, these are hard coded
 # TODO: implement more words to glassary
 for i in phrases:
+    """
+
+ListOfWords = {}
+ListOfOthers = {}
+str = ""
+enum = 0
+
+for item in text:
+    if item.isalpha():
+        str += item.lower()
+    else:
+        if str != "":
+            ListOfWords[enum] = str
+            str = ""
+        else:
+            ListOfOthers[enum] = item;      
+    enum += 1
+
+for item in ListOfWords:
+    if constant.blacklist.get(ListOfWords[item]) != None:
+        ListOfWords[item] = constant.synonym[ListOfWords[item]]
+
+
