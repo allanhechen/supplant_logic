f4e4ec4ac923d49f83c6668ece314e17fa0ac25e
DaDrunkCow <max.yang.6449@gmai.com>
2022-10-15 11:10:34 -0400

    Issue idk how to direct to a file in the current file path directory

--- Python Portion/constant.py
+++ Python Portion/constant.py
@@ -5,6 +5,8 @@ import pygsheets
 split = [", ", ". ", " and ", " but "]
 
 # import glossary from excel sheet
+# TODO: somehow get the file directive after "service_file" to this current opened file
 sheets = pygsheets.authorize(service_file="glossary_key.json").open_by_key("1hsG7UiSjyaIEWVHGd2dHRJIkSrRBeO4IDpQGqp19D9o")
 
+# test
 sheets[0].update_value("test")
