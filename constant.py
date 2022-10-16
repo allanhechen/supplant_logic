60c137c283752b9cd75df5bc7383c2b91e1ea510
xcvk <96631109+xcvk@users.noreply.github.com>
2022-10-16 07:41:43 -0400

    Update constant.py

--- constant.py
+++ constant.py
@@ -4,7 +4,8 @@ blacklist = {"barbarian" : "musular person/agressive (size)",
              "hillbilly" : "ugly person", "lesbo" : "lesbian (lgbtq offensive)", 'dyke' : 'lgbtq offensive',
              "fruity" : "homosexual", "tranny" : "transgender", "retard" : "slow minded", "midget" : "small person", "delusional" : "different minded",
              "deformed" : "disabled", "fatty" : "obese", "lump" : "obese", "sissy" : "homosexual man", "pansy" : "homosexual man", "fairy" : "a homosexual man", 
-             "faggot" : "homosexual man", "twink" :"effeminate man", "stupid": "slow minded"
+             "faggot" : "homosexual man", "twink" :"effeminate man", "stupid": "slow minded", "guys" : "everybody", "homo" : "gay", "garbageman" : "public worker", 
+             "tomboy" : "girl", "tomgirl" : "boy", "girly" : "feminine", "crazy" : "irrational"
              
 }
 
@@ -12,10 +13,11 @@ blacklist = {"barbarian" : "musular person/agressive (size)",
 
 Replacement = { "barbarian" : "agressive horrific human",
                 "chink" : "eastern", "gypsy": "egyptian",
-                "coon" : "Black people", "nigger" : "Black people", "halfbreed" : "multi-ethnicity",
+                "coon" : "Black people", "halfbreed" : "multi-ethnicity",
                 "hillbilly" : "unique looking person", 'lesbo' : 'lesbian', 'dyke' : 'lesbian',
                 'fruity' : "homosexual", 'fairy' : 'homosexual', "midge" : "small person", 
                 "retard" : "slow minded", "midget" : "small person", "delusional" : "different minded",
                 "deformed" : "disabled", "fatty" : "obese", "lump" : "obese", "sissy" : "effeminate man", "pansy" : "a homosexual man", "fairy" : "a homosexual man", 
-                "faggot" : "homosexual man", "twink" :"effeminate man", "tranny" : "transgender","stupid": "slow minded"
+                "faggot" : "homosexual man", "twink" :"effeminate man", "tranny" : "transgender","stupid": "slow minded","guys" : "everybody", "homo" : "gay", "garbageman" : "public worker", 
+                "tomboy" : "girl", "tomgirl" : "boy", "girly" : "feminine", "crazy" : "irrational"
 }
