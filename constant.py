59f9088592b4b3fa058d7f8143cd9a93c9f390d8
xcvk <96631109+xcvk@users.noreply.github.com>
2022-10-16 01:11:40 -0400

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,17 +1,21 @@
-blacklist = {"ABC" : "Western Born Asians (racism)", "ape" : "Black People (racism)", 
-             "faggot" : "Homosexual slur", "barbarian" : "musular person/agressive (size)",
-             "nigger" : "Black People (Racism)", "chink" : "Asians (racism)", "cracker" : "white (racism)",
-             "coon" : "Black People (racism)", "gypsy" : "egyptian (racism)", "half-breed" : "multi-ethnicity",
+blacklist = {"barbarian" : "musular person/agressive (size)",
+             "chink" : "Asians (racism)", 
+             "coon" : "Black People (racism)", "gypsy" : "egyptian (racism)", "halfbreed" : "multi-ethnicity",
              "hillbilly" : "ugly person", "lesbo" : "lesbian (lgbtq offensive)", 'dyke' : 'lgbtq offensive',
-             'fag' : 'lgbtq offensive'
+             "fruity" : "homosexual", "tranny" : "transgender", "retard" : "slow minded", "midget" : "small person", "delusional" : "different minded",
+             "deformed" : "disabled", "fatty" : "obese", "lump" : "obese", "sissy" : "homosexual man", "pansy" : "homosexual man", "fairy" : "homosexual man", 
+             "faggot" : "homosexual man", "twink" :"effeminate man"
+             
 }
 
 
 
-Replacement = { "ABC" : "Western Born Asian",  "ape" : "Black People", 
-                "faggot" : "homoseuxal", "barbarian" : "agressive horrific human",
-                "chink" : "eastern", "cracker" : "western", "gypsy": "egyptian",
-                "coon" : "Black people", "nigger" : "Black people", "half-breed" : "multi-ethnicity",
+Replacement = { "barbarian" : "agressive horrific human",
+                "chink" : "eastern", "gypsy": "egyptian",
+                "coon" : "Black people", "nigger" : "Black people", "halfbreed" : "multi-ethnicity",
                 "hillbilly" : "unique looking person", 'lesbo' : 'lesbian', 'dyke' : 'lesbian',
-                'fag' : "homosexual", 'fairy' : ''
+                'fruity' : "homosexual", 'fairy' : 'homosexual', "midge" : "small person", 
+                "retard" : "slow minded", "midget" : "small person", "delusional" : "different minded",
+                "deformed" : "disabled", "fatty" : "obese", "lump" : "obese", "sissy" : "homosexual man", "pansy" : "homosexual man", "fairy" : "homosexual man", 
+                "faggot" : "homosexual man", "twink" :"effeminate man"
 }
