5a0bbf0e76c3f61d0d5af0701df4956e46244fe7
Max Yang <58752719+DaDrunkCow@users.noreply.github.com>
2022-10-16 01:07:27 -0400

    Update algorithm_placeholder.py

--- algorithm_placeholder.py
+++ algorithm_placeholder.py
@@ -6,32 +6,34 @@ def algor(data):
     
     ListOfWords = {}
     ListOfOthers = {}
-    str = ""
+    result = ""
     enum = 0
 
     for item in data:
         if item.isalpha():
-            str += item.lower()
+            result += item.lower()
         else:
-            if str != "":
-                ListOfWords[enum] = str
+            if result != "":
+                ListOfWords[enum] = result
                 enum += 1
-            str = ""
+            result = ""
 
             ListOfOthers[enum] = item
 
         
             enum += 1
-    ListOfWords[enum] = str
+    ListOfWords[enum] = result
     for item in ListOfWords:
         if constant.blacklist.get(ListOfWords[item]) != None:
             ListOfWords[item] = constant.Replacement[ListOfWords[item]]
 
     enum = 0
-    str = ""
+    result = ""
     while enum != len(ListOfOthers) + len(ListOfWords):
         if enum in ListOfOthers:
-            str += ListOfOthers[enum]
+            result += ListOfOthers[enum]
         else:
-            str += ListOfWords[enum]
+            result += ListOfWords[enum]
         enum += 1
+
+    return result
