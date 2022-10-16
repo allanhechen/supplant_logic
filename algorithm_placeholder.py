96ba5b218f9b5345cea57567d7b6b32b8547602b
xcvk <96631109+xcvk@users.noreply.github.com>
2022-10-16 04:38:49 -0400

    Update algorithm_placeholder.py

--- algorithm_placeholder.py
+++ algorithm_placeholder.py
@@ -1,39 +1,137 @@
 # import constant variables
 import constant
 
-# algorithm
+#  Python String Methods Previous Next Python 
+#  has a set of built-in methods that you can use on strings. 
+#  Note: All string methods returns new values.
+
 def algor(data):
-    
-    ListOfWords = {}
-    ListOfOthers = {}
-    result = ""
-    enum = 0
 
-    for item in data:
-        if item.isalpha():
-            result += item.lower()
-        else:
-            if result != "":
-                ListOfWords[enum] = result
-                enum += 1
-            result = ""
+    # string.split but it includes spaces
+    def splitAndCap(word):
+        hold = []
 
-            ListOfOthers[enum] = item
+        for x in word:
+            hold.append(x)
 
+        hold[0] = hold[0].capitalize()
+
+        hold = "".join(hold)
+
+        return hold
+    # 
+    def holdNchange(L,vow):
+        word = ""
+        if vow == 1:
+            L[0] = "an"
         
-            enum += 1
-    ListOfWords[enum] = result
-    for item in ListOfWords:
-        if constant.blacklist.get(ListOfWords[item]) != None:
-            ListOfWords[item] = constant.Replacement[ListOfWords[item]]
+        for item in L:
+            word += item
 
+        return word
+
+    vowels = {'a','e','i','o','u'}
+    
+    string = ""
+    
+    # enumerate the position of each word and non alphabet characters in dictionary
+    
     enum = 0
-    result = ""
-    while enum != len(ListOfOthers) + len(ListOfWords):
-        if enum in ListOfOthers:
-            result += ListOfOthers[enum]
-        else:
-            result += ListOfWords[enum]
-        enum += 1
+    cap = 0
 
+
+    caphold = {}
+    stringhold = {}
+    otherhold = {}
+
+    # iterate through the string data
+    for char in data:
+        # if it is a alphabet
+        if char.isalpha():
+            # if its a capital then the it will say this word will be capital
+            if not char.islower():
+                cap = 1
+            string += char.lower()
+
+        else:
+            # if its not alphabet and its not an empty string like space bar
+            if string != "":
+                stringhold[enum] = string
+                caphold[enum] = cap
+                cap = 0
+                string = ""
+                enum += 1
+            otherhold[enum] = char
+            enum += 1
+    # add the last word
+    if string.isalpha():
+        stringhold[enum] = string
+        caphold[enum] = cap
+
+    result = ""
+    enum = 0
+    L = []
+    vow = 0
+    # while the length of both dictionary doesnt exceeed the iterator
+    while enum != len(stringhold) + len(otherhold):
+        # if this is a string
+        if enum in stringhold:
+            word = stringhold[enum]
+            # if this is the letter a
+            if word == 'a':
+                vow = 1
+                L.append(word)
+            # if word is sensitive
+            elif word in constant.blacklist:
+                currWord = constant.Replacement[word]
+                # if this is capital
+                if caphold[enum] == 1:
+                    currWord = splitAndCap(currWord)
+                    if vow == 0:
+                        result += currWord
+                    else:
+                        L.append(currWord)
+                        if not currWord[0].lower() in vowels:
+                            vow = 0
+                        result += holdNchange(L,vow)
+                        L = []
+                        vow = 0
+                else:
+                    if vow == 0:
+                        result += currWord
+                    else:
+                        L.append(currWord)
+                        if not currWord[0].lower() in vowels:
+                            vow = 0
+                        result += holdNchange(L,vow)
+                        L = []
+                        vow = 0
+            else:
+                if caphold[enum] == 1:
+                    word = splitAndCap(word)
+                    if vow == 0:
+                        result += word
+                    else:
+                        L.append(word)
+                        if not word[0] in vowels:
+                            vow = 0
+                        result += holdNchange(L,vow)
+                        L = []
+                        vow = 0
+                else:
+                    if vow == 0:
+                        result += word
+                    else:
+                        L.append(splitAndCap(word))
+                        if not word[0] in vowels:
+                            vow = 0
+                        result += holdNchange(L,vow)
+                        L = []
+                        vow = 0
+        else:
+            if vow != 1:
+                result += otherhold[enum]
+            else:
+                L.append(otherhold[enum])
+        enum += 1
     return result
